e9d9cbfd963c8faec546b30718e0cdb924e0b80a
Amreth <amreth@KSHRDs-iMac.local>
2016-12-07 11:38:58 +0700

    Core Data 3

--- Core Data 3/ViewController.swift
+++ Core Data 3/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let student:Student = Student(context:DatabaseController.getContext())
+        student.firstname = "Amrethvicheyo"
+        student.lastname = "Tith"
+        let course:Course = Course(context: DatabaseController.getContext())
+        course.courseName = "iOS"
+        student.addToCourse(course)
+        course.addToStudent(student)
+        
+        DatabaseController.saveContext()
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Student")
+        
+        let result = try? DatabaseController.getContext().fetch(request)
+        
+        for stu in result as![Student]{
+            print(stu.firstname!)
+            print(stu.lastname!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
