93a037d40cf67bcb3d84a5bf3e94ff65b5ee8669
Amreth <amreth@KSHRDs-iMac.local>
2016-12-07 11:49:14 +0700

    did

--- Core Data 3/ViewController.swift
+++ Core Data 3/ViewController.swift
@@ -32,6 +32,10 @@ class ViewController: UIViewController {
             print(stu.lastname!)
         }
         
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
